31acd05ed2b1d5e2896c80753a9c8944025ff632
Sanket Mohapatra <www.sanket1mohapatra@gmail.com>
2021-03-23 11:19:02 +0530

    fix

--- sanpot/__init__.py
+++ sanpot/__init__.py
@@ -19,16 +19,24 @@ class Honeypot(object):
                             filename=logfile,
                             filemode='w')
         self.logger = logging.getLogger(__name__)
-        self.logger.info("Honeyport starting up...")
+        self.logger.info("Honeypot starting up...")
         self.logger.info("Ports: %s" % self.ports)
         self.logger.info("Log file path : %s" % self.logfile)
 
     def handle_connection(self, client_socket, ip, remote_port):
-        req = client_socket.recv(64)
-        self.logger.info("Connection from %s%d : %s" %
-                         (ip, remote_port, req))
-        client_socket.send("Access denied.\n".encode('utf8'))
-        client_socket.close()
+        while True:
+            req = client_socket.recv(64)
+            self.logger.info("Connection from %s:%d : %s" %
+                             (ip, remote_port, req))
+
+            if not req:
+                break
+            elif req == 'b\'killsrv\\n\'':
+                client_socket.close()
+                sys.exit()
+            else:
+                client_socket.send("Access denied.\n".encode('utf8'))
+            # client_socket.close()
 
     def start_listening_thread(self, port):
         listener = socket()
